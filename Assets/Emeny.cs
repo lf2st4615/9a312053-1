1be2edac1c4c59f66c5b8e9a48c253d87386c9ad
9a312053 <lf2st4615@yahoo.com.tw>
2019-01-15 20:10:54 +0800

    first commit

---
+++ Assets/Emeny.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emeny : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col) //碰撞事件
+    {
+        if (col.tag == "Emeny") //如果標籤是Emeny
+        {
+            Destroy(col.gameObject); //消滅碰撞的物件
+        }
+    }
+    void AnimationEnd()
+    {
+        Destroy(gameObject); //消滅物件
+    }
+}
